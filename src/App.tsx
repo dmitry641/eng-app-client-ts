62f1841140bb2870897fee3e22f47da1c81f6f4e
Dmitry <dmitry6416@gmail.com>
2022-07-25 19:46:22 +0300

    manifest icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="App">Test</div>;
+  const [state, setState] = useState(0);
+  return (
+    <div>
+      <div>Test: {state}</div>
+      <div>
+        <button onClick={() => setState((prev) => prev + 1)}>plus</button>
+        <button onClick={() => setState((prev) => prev - 1)}>minus</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
