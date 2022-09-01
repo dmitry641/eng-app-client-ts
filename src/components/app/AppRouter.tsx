84cd39d2a19b855d7e1dce3937b0080b493be943
Dmitry <dmitry6416@gmail.com>
2022-09-01 14:20:16 +0300

    fix imports

--- src/components/app/AppRouter.tsx
+++ src/components/app/AppRouter.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import "react-toastify/dist/ReactToastify.css";
 import { useAppSelector } from "../../hooks/useAppSelector";
 import { privateRoutes, publicRoutes, RoutesEnum } from "../../routes";
 import { ModulesInit } from "./ModulesInit";
